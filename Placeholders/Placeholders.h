b202931d55e01f216770cf82fec7e6ff5b46d27f
Oleg Dreyman <dreymonde@me.com>
2017-04-13 14:02:53 +0300

    Initial Commit

---
+++ Placeholders/Placeholders.h
@@ -0,0 +1,19 @@
+//
+//  Placeholders.h
+//  Placeholders
+//
+//  Created by Олег on 13.04.17.
+//  Copyright © 2017 AnySuggestion. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Placeholders.
+FOUNDATION_EXPORT double PlaceholdersVersionNumber;
+
+//! Project version string for Placeholders.
+FOUNDATION_EXPORT const unsigned char PlaceholdersVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Placeholders/PublicHeader.h>
+
+
